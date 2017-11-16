1ef62418fedcc7e75c9a46e184aec06e3ae0e760
tjmitchell1 <botulinusrogue@gmail.com>
2017-11-16 00:45:31 -0500

    Edit item stat types. Add legendary Effects. Add notes on how stats, status, and effects work.

---
+++ src/server/server.js
@@ -0,0 +1,16 @@
+/**
+ * API Calls directly to DB.
+ * 
+ * Create Character
+ * Get Character list
+ * Save Character
+ * Delete Character
+ * 
+ * Get Weapon
+ * Get Armor
+ * Get Consumable
+ * Get Key Item
+ * 
+ * Get Full Skill Tree
+ * 
+ */
